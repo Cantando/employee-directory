fb17a72c2aa6657a747e8e6718f5a0a5694c5268
Cantando <65988365+Cantando@users.noreply.github.com>
2020-08-10 20:32:22 -0400

    Updates

---
+++ precache-manifest.0797afbbb35204d1f620d6526f42f7b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "237a31e21699a89cb37e9adf08a97c10",
+    "url": "/employee-directory/index.html"
+  },
+  {
+    "revision": "ece41b5c356cceb44ec2",
+    "url": "/employee-directory/static/css/main.ef59ea11.chunk.css"
+  },
+  {
+    "revision": "c5906de4b9aea3ae0c7d",
+    "url": "/employee-directory/static/js/2.58366293.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/employee-directory/static/js/2.58366293.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ece41b5c356cceb44ec2",
+    "url": "/employee-directory/static/js/main.f4e57ed7.chunk.js"
+  },
+  {
+    "revision": "79d7c4f4ad2a4d373d4e",
+    "url": "/employee-directory/static/js/runtime-main.759497ab.js"
+  }
+]);
